abd1b42451007ca25ad4868120fb1714966fcdc3
Rv607 <jrobah@gmail.com>
2020-10-19 12:47:47 +0000

    some cleaning of dataset

--- model.py
+++ model.py
@@ -8,3 +8,16 @@ from sklearn import preprocessing
 df = pd.read_csv("data.csv")
 
 df = df[['isLegendary','Generation', 'Type_1', 'Type_2', 'HP', 'Attack', 'Defense', 'Sp_Atk', 'Sp_Def', 'Speed','Color','Egg_Group_1','Height_m','Weight_kg','Body_Style']]
+
+# converting bolean text to int
+df['isLegendary'] = df['isLegendary'].astype(int)
+
+def dummy_creation(df, dummy_categories):
+    for i in dummy_categories:
+        df_dummy = pd.get_dummies(df[i])
+        df = pd.concat([df,df_dummy],axis=1)
+        df = df.drop(i, axis=1)
+    return(df)
+
+df = dummy_creation(df, ['Egg_Group_1', 'Body_Style', 'Color','Type_1', 'Type_2'])
+
